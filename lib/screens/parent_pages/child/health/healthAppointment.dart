45fc91298b675ab50b608f7eaf0bf0ffc9d10538
Shawn-3604 <maizat@gmail.com>
2023-12-05 03:28:38 +0800

    Add dependency

--- lib/screens/parent_pages/child/health/healthAppointment.dart
+++ lib/screens/parent_pages/child/health/healthAppointment.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class healthAppointment extends StatefulWidget {
   const healthAppointment({super.key, required this.childId});
@@ -10,6 +11,15 @@ class healthAppointment extends StatefulWidget {
 }
 
 class _healthCalendarState extends State<healthAppointment> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+  Map<DateTime, List<String>> _events = {
+    DateTime.now(): ['Meeting at 10 AM', 'Lunch with Client'],
+    DateTime.now().add(Duration(days: 2)): ['Team Huddle'],
+    DateTime.now().add(Duration(days: 5)): ['Conference Call'],
+  };
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
